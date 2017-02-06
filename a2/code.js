591f474e0d96080cd12d76ec079db44987d7df15
jackieluc <jackie_luc@hotmail.com>
2017-02-06 01:54:11 -0700

    added palindromes, ten longest words

--- a2/code.js
+++ a2/code.js
@@ -1,6 +1,3 @@
-// RegEx - everything that is not alphanumeric
-punctuations = /\W/;
-
 /**
  * @param str - some block of text
  * @return a boolean based on the input parameter
@@ -19,7 +16,8 @@ function getWords(txt) {
     while(txt.includes("'")) 
         txt = txt.replace("'", "");
     
-    return txt.split(punctuations);
+    // RegEx - everything that is not alphanumeric
+    return txt.split(/\W/);
 }
 
 /**
@@ -115,11 +113,40 @@ function getMaxLineLength(txt) {
 }
 
 /**
+ * 1. splits the word into an array of characters
+ * 2. reverses the array
+ * 3. joins the array of characters into a string
+ * @param word - some text
+ * @return the reverse of the input paramater
+*/
+function reverseWord(word) {  
+    return word.split("").reverse().join("");
+}
+
+/**
  * @param txt - some block of text
- * @return an array of palindromes
+ * @return an array of unique palindromes
 */
 function getAllPalindromes(txt) {
+    let words = getWords(txt);
+    let palindromes = [];
     
+    words.forEach( function(val, index, words) {
+        // only consider words with more than 2 characters
+        if (val.length > 2) {
+            
+            let lowerCase = val.toLowerCase();
+            
+            // it is a palindrome if the word reads the same forwards and backwards
+            if (lowerCase === reverseWord(lowerCase)) {
+                
+                // add to palindromes array only if it is unique
+                if (!palindromes.includes(lowerCase))
+                    palindromes.push(lowerCase);
+            }
+        }
+    });
+    return palindromes;
 }
 
 /**
@@ -127,14 +154,35 @@ function getAllPalindromes(txt) {
  * @return an array of the ten longest words of the input parameter
 */
 function getTenLongestWords(txt) {
+    let words = getWords(txt); 
     
+    // sort in according to descending order 
+    // if the two words are of equal length, we compare alphabetically
+    let sorted = words.sort( function(firstWord, secondWord) {
+        return secondWord.length - firstWord.length || firstWord.localeCompare(secondWord);
+    });
+    
+    // return the 10 longest words
+    if (sorted.length >= 10)
+        return sorted.slice(0, 10);
+    else {
+        let shortArray = [];
+        
+        // return the number of longest words up to 9
+        // we don't care about empty strings
+        sorted.forEach( function(val, index, splitted) {
+            if (val !== "") 
+                shortArray.push(val);
+        });
+        
+        return shortArray;
+    }
 }
 
 /**
  * @param txt - some block of text
  * @return an array of the ten most frequent words of the input parameter
 */
-
 function getTenMostFrequentWords(txt) {
     
 }
@@ -147,8 +195,8 @@ function getStats(txt) {
         nNonEmptyLines: getNonEmptyLinesNumber(txt),
         averageWordLength: getAverageWordLength(txt),
         maxLineLength: getMaxLineLength(txt),
-        allPalindromes: ["123", "kayak", "mom"],
-        tenLongestWords: ["xxxxxxxxx", "123444444"],
+        allPalindromes: getAllPalindromes(txt),
+        tenLongestWords: getTenLongestWords(txt),
         tenMostFrequentWords: ["a", "this", "the"]
     };
 }
